394c86a414f302dd005c51ac6628b49eaacfbb31
Diego Morales <dgmr85@gmail.com>
2015-06-11 21:43:31 -0500

    Adding EF 6 with API

---
+++ BookService/BookService/Scripts/app.js
@@ -0,0 +1,30 @@
+﻿var ViewModel = function() {
+    var self = this;
+    self.books = ko.observableArray();
+    self.error = ko.observable();
+
+    var booksuri = '/api/books/';
+
+    function ajaxHelper(uri, method, data) {
+        self.error('');
+        return $.ajax({
+            type: method,
+            url: uri,
+            dataType: 'json',
+            contentType: 'application/json',
+            data: data ? JSON.stringify(data) : null
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            self.error(errorThrown);
+        });
+    }
+
+    function getAllBooks() {
+        ajaxHelper(booksuri, 'GET').done(function(data) {
+            self.book(data);
+        });
+    }
+
+    getAllBooks();
+};
+
+ko.applyBindings(new ViewModel());
--- GeekQuiz/GeekQuiz/Scripts/app/quiz-controller.js
+++ GeekQuiz/GeekQuiz/Scripts/app/quiz-controller.js
@@ -16,7 +16,7 @@
             $scope.title = "loading question...";
             $scope.options = [];
 
-            $http.get("/api/trivia").success(function(data, status, headers, config) {
+            $http.get('/api/trivia').success(function(data, status, headers, config) {
                 $scope.options = data.options;
                 $scope.title = data.title;
                 $scope.answered = false;
@@ -31,8 +31,8 @@
             $scope.working = true;
             $scope.answered = true;
 
-            $http.post("/api/trivia", { "question": option.questionId, "option": option.Id }).success(function(data, status, headers, config) {
-                $scope.correctAnswer = (data == "true");
+            $http.post('/api/trivia', { 'questionId': option.questionId, 'optionId': option.id }).success(function(data, status, headers, config) {
+                $scope.correctAnswer = angular.equals(data,true);
                 $scope.working = false;
             }).error(function(data, status, headers, config) {
                 $scope.title = "Oops...something went wrong";
