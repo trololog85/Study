9dbcc57af8055c22f11876e08f3768ca5eca4bcf
diego <dgmr85@gmail.com>
2015-06-25 19:27:23 -0500

    Adding products app

---
+++ ProductsApp/ProductsApp/ProductsApp/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ProductsApp.Models;
+
+namespace ProductsApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private Product[] products = {
+            new Product{Id=1,Name="Tomato Soup",Category = "Groceries",Price=1},
+            new Product{Id=2,Name="Yo-yo",Category = "Toys",Price=3.75m},
+            new Product{Id=3,Name="Hammer",Category = "Hardware",Price = 16.99m}
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+
+            if (product == null)
+                return NotFound();
+
+            return Ok(product);
+        }
+    }
+}
